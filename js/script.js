4093a6409a6738a50bdb4ab0420a35c6d1e964ab
prophet12505 <1250523689@qq.com>
2023-04-13 14:49:17 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+//set up hover effect:
+$(".image-gallery-home article").on("mouseover", function() {
+  console.log("mose is over");
+  $(this).find(".div-filter").css("display", "inherit");
+  $(this).find("h4").css("display", "inherit");
+});
+$(".image-gallery-home article").on("mouseout", function() {
+  console.log("mose is out");
+  $(this).find(".div-filter").css("display", "none");
+  $(this).find("h4").css("display", "none");
+});
+
+
+
+
+
+
+
+
+
+
+
+navStatus=false;//false means nav is closed(hide), vice versa
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  $("nav").css("top","0");
+  console.log("open");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  $("nav").css("top","-100vh");
+  console.log("close!");
+}
+
+function handleHamburgerClick(){
+  if(!navStatus)openNav();
+  else closeNav();
+  navStatus=!navStatus;
+}
+
+
